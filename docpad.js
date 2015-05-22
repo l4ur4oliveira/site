ed45a84f0ef21f0cd93b59b6710fe3b14cfbecfe
unknown <laura@PMDPOA024.pmweb.local>
2015-05-22 10:25:53 -0300

    Create branch master with dev files

---
+++ docpad.js
@@ -0,0 +1,115 @@
+module.exports = {
+
+  prompts: false,
+
+  // These are variables will be accessible via our templates
+  templateData: {
+
+    // Conference info
+    conf: {
+      name: "Conference name",
+      description: "Conference description",
+      date: "November 15",
+      // If your event is free, just comment this line
+      price: "$100",
+      venue: "Coco Bongo",
+      address: "Boulevard Kukulcan, 30",
+      city: "Cancún",
+      state: "Quintana"
+    },
+
+    // The Call To Action button at the header,
+    // If you don't want this, just remove the callToAction property.
+    callToAction: {
+        text: "Register now!",
+        link: "http://eventick.com.br"
+    },
+
+    // "Fork me on GitHub", if you don't want this, just remove the forkButton property
+    forkButton: {
+        // repository: "https://github.com/braziljs/conf-boilerplate"
+    },
+
+    // Site info
+    site: {
+      theme: "yellow-swan",
+      // url: "http://braziljs.github.io/conf-boilerplate/",
+      // googleanalytics: "UA-33656081-1"
+    },
+
+    // Active sections on the website
+    // to deactivate comment out with '//'
+    // you can also change order here and it will reflect on page
+    sections: [
+      'about'
+      // 'location',
+      // 'speakers',
+      // 'schedule',
+      // 'sponsors',
+      // 'partners'
+      // 'contact'
+    ],
+
+    // Labels which you can translate to other languages
+    labels: {
+      about: "About",
+      location: "Location",
+      speakers: "Speakers",
+      schedule: "Schedule",
+      sponsors: "Sponsors",
+      partners: "Partners",
+      contact: "Contact"
+    },
+
+    // The entire schedule
+    schedule: [
+      {
+        name: "Check-in / Breakfast",
+        time: "9h00"
+      },
+      {
+        name: "Linus Torvalds",
+        photo: "themes/yellow-swan/img/speaker.jpg",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+        company: "Linux Foundation",
+        link: {
+          href: "http://twitter.com/linus",
+          text: "@linus"
+        },
+        presentation: {
+          title: "Digging into a Linux Kernel",
+          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+          time: "10h00"
+        }
+      }
+    ],
+
+    // List of Sponsors
+    sponsors: [
+      {
+        name: "Eventick",
+        logo: "themes/yellow-swan/img/sponsor.png",
+        url: "http://eventick.com.br"
+      }
+    ],
+
+    // List of Partners
+    partners: [
+      {
+        name: "BrazilJS",
+        logo: "themes/yellow-swan/img/partner.png",
+        url: "http://braziljs.org"
+      }
+    ],
+
+    // Theme path
+    getTheme: function() {
+      return "themes/" + this.site.theme;
+    },
+
+    // Site Path
+    getUrl: function() {
+    	return this.site.url;
+    }
+  }
+};
